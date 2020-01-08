6cf43acba3986449eb9c52b9d18873ebabc8c0f6
R.R.Arunkumar <54265857+Arunkumar-RR@users.noreply.github.com>
2020-01-08 08:01:22 +0530

    Add files via upload

---
+++ show.js
@@ -0,0 +1,75 @@
+function displayRadioValue1(q1) 
+{ 
+     
+  var val= document.getElementsByName("q1");
+  for(var i=0;i<val.length;i++)
+  {
+      if(val[i].checked)
+      {
+          
+        console.log(val[i].value);
+        break;
+      }
+       
+  }
+} 
+function displayRadioValue2(q2) 
+{ 
+     
+  var val= document.getElementsByName("q2");
+  for(var i=0;i<val.length;i++)
+  {
+      if(val[i].checked)
+      {
+          
+        console.log(val[i].value);
+        break;
+      }
+       
+  }
+} 
+function displayRadioValue3(q3) 
+{ 
+     
+  var val= document.getElementsByName("q3");
+  for(var i=0;i<val.length;i++)
+  {
+      if(val[i].checked)
+      {
+          
+        y=val[i].value;
+        console.log(y);
+  }
+      }
+      
+} 
+function displayRadioValue4(q4) 
+{ 
+     
+  var val= document.getElementsByName("q4");
+  for(var i=0;i<val.length;i++)
+  {
+      if(val[i].checked)
+      {
+          
+        x=val[i].value;
+        console.log(x);
+      }
+       
+  }
+} 
+function displayRadioValue5(q5) 
+{ 
+     
+  var val= document.getElementsByName("q5");
+  for(var i=0;i<val.length;i++)
+  {
+      if(val[i].checked)
+      {
+          
+        console.log(val[i].value);
+        break;
+      }
+       
+  }
+} 
